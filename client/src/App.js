e3f2cba891dd8d666016c2f2924f97f286d0faa1
Nguyenttm0907 <93388452+Nguyenttm0907@users.noreply.github.com>
2022-11-19 14:30:25 +0700

    create search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import { SignIn, WithAuth } from "./conponents/login";
+import {Search} from "./conponents/search"
 import {Post} from './conponents/post'
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<p>This is the home page</p>} />
+            <Route path="/" element={<Search />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/create_post" element={<WithAuth><Post /></WithAuth>} />
         </Routes>
---
+++ client/src/conponents/search/search.jsx
@@ -0,0 +1,45 @@
+import { React, memo, useState} from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import axios from "axios";
+
+function Search() {
+    const [location, setLocation] = useState('');
+    const handleSearch = async (e) => {
+        try {
+            
+            const res = await axios.get(`http://localhost:4000/api/post/search?location=${location}`);
+            console.log(res);
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+            <Box
+                noValidate
+                sx={{marginTop: '10px', display:'flex', justifyContent:'center', alignItems:'center'}}
+            >
+                <TextField
+                    required
+                    id="search"
+                    label="Enter location..."
+                    name="search"
+                    sx={{width: '30%'}}
+                    onChange={(e) => setLocation(e.target.value)}
+                />
+                
+                <Button
+                    type="submit"
+                    variant="contained"
+                    onClick={handleSearch}
+                    sx={{height:'55px',ml: 1}}
+                >
+                    Search
+                </Button>
+            </Box>    
+    )
+}
+export default memo(Search);
