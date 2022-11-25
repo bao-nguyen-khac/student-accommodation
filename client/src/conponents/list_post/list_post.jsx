50cb6910e7eb076ae40dedab16b05829fc8100d7
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2022-11-25 16:13:27 +0700

    update listPost.js
    kinda fully CSS listpost

--- client/src/conponents/list_post/list_post.jsx
+++ client/src/conponents/list_post/list_post.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Avatar } from "@mui/material";
 import { Container } from "@mui/system";
-import axios from "axios";
+import './index.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -10,11 +10,7 @@ const Listpost = () => {
     
     // fetchData()
     const [posts, setPosts] = useState()
-    const ObjectRow = () => {
-        <Box sx={{padding: '20px'}}>
-            hehe
-        </Box>
-    }
+    
     useEffect(()=>{
         async function fetchData() {
             fetch("http://localhost:4000/api/post/get")
@@ -27,31 +23,60 @@ const Listpost = () => {
 
     
   return (
-    <Container sx={{width: "60%", margin: "30px auto"}}>
+    <Container sx={{
+        display: 'flex', 
+        justifyContent: 'center',
+        backgroundColor: 'whitesmoke'
+        }}>
        {posts?.map(element =>
-            <Box sx={{border: '1px solid black', margin: '20px'}}>
-                
-
-                    <h3 id={element.title}>{element.title}</h3>
-                    
-                    <Typography 
-                    id={element.desc} >
-                        {element.desc}
-                    </Typography>
-                    
-                    <Typography style={{fontStyle: 'italic', justifyContent: 'flex-end', fontSize: '15px'}}
-                    id={element.location}>
-                        {element.location}
-                    </Typography>
-                    <Typography id={element.price}>
-                        {element.price}
-                    </Typography>
-                    <Typography id={element.status} sx={{fontStyle: 'bold', fontWeight: '20px'}}>
-                        {element.status}
-                    </Typography>
-                    <img style={{width: '50%', height: '50%'}} src={element.imageURL} alt="picture"></img>
-                    
-
+            <Box sx={{
+                border: '1px solid black',
+                borderRadius: '8px',
+                margin: '20px',
+                width: '50%',
+                padding: '15px',
+            }}>
+            <span>
+                {/* <img className="avatar" src="https://www.w3schools.com/howto/img_avatar.png"></img> */}
+                <Avatar src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+            </span>
+            <span className="userPost">
+                Huy Bùi
+            </span>
+            <span className="imagePost">
+                <img src={element.imageURL} alt='picture'/>
+            </span>
+            <span className="titlePost">
+                {element.title}
+            </span>
+            <span>
+                Tình trạng: 
+                <span style={{
+                    color: text === 'EMPTY' ? "red" : "rgb(11, 173, 73)", 
+                    fontWeight: 'bold'
+                }}>
+                {element.status}
+                </span> 
+            </span>
+            <span className="pricePost">
+                {element.price}
+            </span>
+            <span className="locationPost">
+                {element.location}
+            </span>
+            <span className="descriptionPost">
+                <p>
+                    <h4 style={{margin: '0'}}>Mô tả:</h4>
+                    {element.description || <Typography>Phòng khá rộng nên ở được từ 3-4 người. Miễn phí gửi xe, miễn phí wifi... Phòng sạch sẽ, trang bị full toàn bộ nội thất chỉ cần xách valo vào ở ngay!!</Typography>}
+                    <a href="#" style="font-style: italic; font-size: 13px;">Xem chi tiết</a>
+                </p>
+            </span>
+            <span style={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+            }}>
+                <Button variant="outlined" href='#'>Đặt cọc ngay</Button>
+            </span>
             </Box>
         )}
     </Container>
